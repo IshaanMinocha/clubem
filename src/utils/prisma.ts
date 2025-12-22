4a43738dfee78bc27248facebe719dabd80ec9ee
Ishaan Minocha <minochaishaan2003@gmail.com>
2025-12-23 02:10:35 +0530

    prisma vercel error resolve

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,6 +1,14 @@
 import "dotenv/config";
 import { PrismaClient } from '../../generated/prisma/client'
 
-const prisma = new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
-export { prisma }
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
